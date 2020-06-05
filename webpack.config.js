e63615b1f6a7643494038fa53f16de09b0d3316f
Tony Spataro <xeger@xeger.net>
2020-06-05 15:00:21 -0700

    ts - upgrade everything; remove ForkTsCheckerWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
+/* eslint-env node */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 const path = require('path');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src'),
 
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
 
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'jsx', '.json']
+    extensions: ['.ts', '.tsx', '.js', 'jsx', '.json'],
   },
 
   module: {
     rules: [
-      { test: /\.(ts|js)x?$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
+      { test: /\.(ts|js)x?$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ],
   },
-
-  plugins: [new ForkTsCheckerWebpackPlugin()]
 };
