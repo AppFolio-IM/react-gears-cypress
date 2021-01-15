69c9e9b24c3c31182552ff29b5b739c31e7f05ea
Tony Spataro <xeger@xeger.net>
2021-01-15 13:47:38 -0800

    Actually export sel

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import * as commands from './commands';
 import * as find from './find';
 import * as match from './match';
 import * as hasManyFields from './hasManyFields';
+import * as sel from './sel';
 
 export * from './components';
 export * from './interfaces';
 
-export { commands, find, match, hasManyFields };
+export { commands, find, match, hasManyFields, sel };
