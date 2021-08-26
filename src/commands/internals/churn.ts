5f37a7f333a0b9cfe5b25f8f90ca441a70ecfcc5
Hillary Hueter <hillary.hueter@appfolio.com>
2021-08-26 10:47:12 -0700

    hh - remove stray console.log

--- src/commands/internals/churn.ts
+++ src/commands/internals/churn.ts
@@ -84,7 +84,6 @@ function requeryOne(this: Window, el: HTMLElement, _i: number) {
 export function requeryDetached($q: JQuery<HTMLElement>) {
   // HACK: I got tired of paying the TypeScript tax and used `as`
   // TODO: remove hack & do it right
-  console.log('Requery Detatched', $q);
   return Cypress.$(Cypress.$.map($q, requeryOne)) as JQuery<HTMLElement>;
 }
 
