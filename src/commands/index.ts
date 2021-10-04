f0504193ad1358241d1e096ec5b46e7e6989c0f7
Hillary Hueter <hillary.hueter@appfolio.com>
2021-10-04 10:55:01 -0700

    Add the click overwrite to rgc commands.  (#29)
    * 5.21.0
    
    * hh/ts - enhance requery and churn metrics

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,10 +2,11 @@ import { clear } from './clear';
 import { component } from './component';
 import { fill } from './fill';
 import { select } from './select';
+import { click } from './click';
 
-export { clear, component, fill, select };
+export { clear, component, fill, select, click };
 
-type CommandName = 'clear' | 'fill' | 'component' | 'select';
+type CommandName = 'clear' | 'fill' | 'component' | 'select' | 'click';
 
 /**
  * Register Cypress commands provided by this package. Some commands are new and
@@ -27,4 +28,6 @@ export function add(...names: CommandName[]) {
     Cypress.Commands.add('component', { prevSubject: 'optional' }, component);
   if (all || names.includes('select'))
     Cypress.Commands.overwrite('select', select);
+  if (all || names.includes('click'))
+    Cypress.Commands.overwrite('click', click);
 }
