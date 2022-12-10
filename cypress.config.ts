ee293a25de8c8208f0cdea1f7b80ce9153737a1a
Tony Spataro <github.com@tracker.xeger.net>
2022-12-10 06:13:51 -0800

    Speed up cypress CLI runs; mute tsconfig spam

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
     setupNodeEvents(on, config) {},
+    video: false,
     viewportWidth: 640,
     viewportHeight: 480,
-    specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
+    specPattern: 'cypress/component/**/*.cy.{js,ts,jsx,tsx}',
     devServer: {
-      framework: "react",
-      bundler: "webpack",
+      framework: 'react',
+      bundler: 'webpack',
     },
   },
 });
