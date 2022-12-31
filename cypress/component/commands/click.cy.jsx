9397c1feb81afc89e8c2978c0c311219644ba90a
Tony Spataro <github.com@tracker.xeger.net>
2022-12-31 11:53:06 -0800

    Properly type commands/click

---
+++ cypress/component/commands/click.cy.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  BlockPanel,
+  Button,
+  Icon,
+} from '@appfolio/react-gears';
+
+// NB: this package will stop overriding cy.click; the test is still useful to
+// prove that Cypress behavior does not regress (thereby requiring an override again).
+describe('cy.click', () => {
+  it('deals with FontAwesome buttons', () => {
+    cy.mount(
+      <div>
+        <Button><Icon name="times"/></Button>
+      </div>
+    );
+
+    cy.get('button').click();
+  });
+});
--- src/commands/click.ts
+++ src/commands/click.ts
@@ -1,15 +1,22 @@
-type ClickFn = (
-  subject: JQuery,
-  options?: Partial<Cypress.ClickOptions>
-) => Cypress.Chainable;
+import { isQuery } from './internals/constants';
 
+/**
+ * Click something and deal with empty buttons that contain only an icon.
+ * @deprecated this is not necessary since Cypress 10
+ */
 export function click(
-  originalClick: ClickFn,
-  subject: JQuery,
+  this: Mocha.Context,
+  originalFn: Cypress.CommandOriginalFnWithSubject<'click', unknown>,
+  prevSubject: unknown,
+  x: number,
+  y: number,
   options?: Partial<Cypress.ClickOptions>
 ) {
-  if (subject.is('button') && subject.text() === '') {
-    return originalClick(subject, { ...options, force: true });
+  if (!isQuery(prevSubject)) {
+    return originalFn(prevSubject, x, y, options);
   }
-  return originalClick(subject, options);
+  if (prevSubject.is('button') && prevSubject.text() === '') {
+    return originalFn(prevSubject, x, y, { ...options, force: true });
+  }
+  return originalFn(prevSubject, x, y, options);
 }
