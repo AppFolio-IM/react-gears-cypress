c13fc45526deb0b68cf89602a23b58a9af84eaae
Tony Spataro <github.com@tracker.xeger.net>
2022-12-31 11:45:15 -0800

    Properly type commands/clear

--- src/commands/internals/constants.ts
+++ src/commands/internals/constants.ts
@@ -3,3 +3,5 @@
 export const FORCE_QUICK_QUIET = { force: true, delay: 1, log: false };
 export const FORCE_QUIET = { force: true, log: false };
 export const QUIET = { log: false };
+
+export const isQuery = (u: unknown): u is JQuery => typeof u === 'object' && u && (u as any).jquery;
