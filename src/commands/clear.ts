ea5a90d2214b8c3f88d9c22ae22aba1db99b681b
Tony Spataro <github.com@tracker.xeger.net>
2022-12-31 13:27:33 -0800

    Remove useless this typings

--- src/commands/clear.ts
+++ src/commands/clear.ts
@@ -5,7 +5,6 @@ import { blurIfNecessary, dismissAriaPopup } from './internals/interaction';
  * Clear a vanilla HTML input or a fancy gears component e.g. Select.
  */
 export function clear(
-  this: Mocha.Context,
   originalFn: Cypress.CommandOriginalFnWithSubject<'clear', unknown>,
   prevSubject: unknown,
   options?: Partial<Cypress.ClearOptions>
