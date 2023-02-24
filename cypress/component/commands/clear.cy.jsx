106b45886a11b8716639ee355b504e14ff0b392f
Tony Spataro <github.com@tracker.xeger.net>
2023-02-24 10:11:46 -0800

    Simplify + harden cy.fill of Combobox. (#117)
    * Simplify + harden cy.fill of Combobox.
    Switch to "clear then choose" heuristic, identical to Select.
    Add robustness tests.
    
    * Remove extraneous .focus() from fill!
    
    * Use Combobox automation hooks with clear

--- cypress/component/commands/clear.cy.jsx
+++ cypress/component/commands/clear.cy.jsx
@@ -93,6 +93,74 @@ describe('cy.clear', () => {
       cy.component(comp.Combobox, 'some label').clear();
       cy.get('[data-testid=combobox-menu]').should('not.be.visible');
     });
+
+    it('cooperates with fill', () => {
+      let selected = 'alpha';
+
+      cy.mount(
+        <Testbed
+          initialValue={selected}
+          onChange={(v) => {
+            selected = v;
+          }}
+        />
+      );
+
+      cy.component(comp.Combobox, 'some label').fill('alpha');
+      eventually(() => selected === 'alpha');
+      cy.component(comp.Combobox, 'some label').clear();
+      eventually(() => selected === undefined);
+    });
+
+    context('with test-automation hooks', () => {
+      function FutureTestbed({ clearable = true, onClear, onToggle }) {
+        return (
+          <FormLabelGroup label="some label">
+            <div data-testid="combobox-dropdown" class="combobox dropdown">
+              <div aria-haspopup="true" aria-expanded="false">
+                <div class="input-group">
+                  <input data-testid="combobox-input" class="form-control" placeholder="Select..." />
+                  <button disabled={!clearable} type="button" onClick={onClear} data-testid="combobox-clear" title="Clear value" class="bg-transparent border-0 font-weight-bold px-0 btn btn-secondary disabled" style={{ opacity: 0 }}>×</button>
+                  <button type="button" onClick={onToggle} data-testid="combobox-caret" class="bg-transparent border-0 pl-0 pr-2 btn btn-secondary" aria-label="Toggle options menu"><i aria-hidden="true" class="fa fa-caret-down fa-fw"></i></button>
+                </div>
+              </div>
+            </div>
+          </FormLabelGroup >
+        );
+      }
+
+      it('uses the clear button', () => {
+        let cleared = false;
+
+        cy.mount(
+          <FutureTestbed
+            onClear={() => {
+              cleared = true;
+            }}
+          />
+        );
+
+        cy.component(comp.Combobox, 'some label').clear();
+        eventually(() => cleared === true);
+      })
+
+      it('tolerates disabled clear button', () => {
+        let cleared = false;
+
+        cy.mount(
+          <FutureTestbed
+            clearable={false}
+            onClear={() => {
+              cleared = true;
+            }}
+          />
+        );
+
+        cy.component(comp.Combobox, 'some label').clear();
+        cy.wait(1000);
+        expect(cleared).to.equal(false);
+      })
+    })
   });
 
   context('Select component', () => {
